24fc7cca206629d5680a3213e782a81cfec95b56
tona44 <kuryu.waku@gmail.com>
2021-08-21 07:37:07 +0000

    app2-3 画像投稿機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters,if: :devise_controller?
 
-  protected
-  # protectedは他のコントローラからも参照可（privateは自分のコントロール内のみ）
+def after_sign_in_path_for(resource)
+  post_images_path
+end
+
+
+  protected  # protectedは他のコントローラからも参照可（privateは自分のコントロール内のみ）
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '206569f2677498d7ba55b88f97616a23b9b77b0aceec951d1b6811575b8dabbdb533ac718e5062fec98473eeff9ca702ad0f31f742449a1cd9777223a98ae38c'
